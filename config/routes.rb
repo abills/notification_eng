36d70f9b5866fabeb9a2647f98d1cea7ec72fea4
Andrew Bills <andrew.bills@mincom.com>
2012-05-08 13:26:55 +1000

    Added Controller for User & Routes properly

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ NotificationEng::Application.routes.draw do
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  resources :users
 
   # Sample resource route with options:
   #   resources :products do
