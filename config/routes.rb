4a6736d89b37208a53de00820da0f06c899a3d18
Andrew Bills <andrew.bills@mincom.com>
2012-05-11 18:07:50 +1000

    added rules to subscription parent child routing

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,10 @@ NotificationEng::Application.routes.draw do
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-  resources :users, :rules
+  resources :users
+  resources :subscriptions do
+    resources :rules
+  end
 
   # Sample resource route with options:
   #   resources :products do
